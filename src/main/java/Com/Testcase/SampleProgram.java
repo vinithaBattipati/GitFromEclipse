baead8d341017574b74eb08489dc4314a121e590
we <we@192.168.1.5>
2022-03-27 05:46:47 -0700

    First commit.....

---
+++ src/main/java/Com/Testcase/SampleProgram.java
@@ -0,0 +1,20 @@
+package Com.Testcase;
+
+import org.testng.annotations.Test;
+
+public class SampleProgram {
+	@Test
+	
+	public void method() {
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("Do work");
+		}
+			
+		
+	}
+
+}
